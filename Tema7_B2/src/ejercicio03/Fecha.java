97d2234cde4601b4a2557190725efc259ba307a8
ikloerzer <ikloerzer@106-23.info2>
2023-03-28 12:34:46 +0200

    Faltan los comentarios y hacer bien algunos main

--- Tema7_B2/src/ejercicio03/Fecha.java
+++ Tema7_B2/src/ejercicio03/Fecha.java
@@ -1,19 +1,137 @@
 package ejercicio03;
 
 public class Fecha {
-	
+
 	private int dia;
 	private int mes;
 	private int año;
-	
+
 	public Fecha() {
-		
+
 	}
+
+	public Fecha(int dia, int mes, int año) {
+
+		this.dia = dia;
+		this.mes = mes;
+		this.año = año;
+	}
+
+	public int getDia() {
+		return dia;
+	}
+
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+
+	public int getMes() {
+		return mes;
+	}
+
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+
+	public int getAño() {
+		return año;
+	}
+
+	public void setAño(int año) {
+		this.año = año;
+	}
+
+	public boolean esBisiesto() {
+
+		boolean bisiesto = false;
+
+		if (getAño() % 4 == 0 || (getAño() % 100 == 0 && getAño() % 400 == 0)) {
+			bisiesto = true;
+		}
+
+		return bisiesto;
+
+	}
+
+	public boolean fechaCorrecta() {
+
+		boolean fechaC = false;
+
+		if (getDia() >0 || getDia() <= 31) {
+
+			if (getMes() == 2 && getDia() <= 29 && esBisiesto()) {
+
+				fechaC = true;
+
+			}
+			
+			if (getMes() <= 12 && getMes() > 0) {
+
+				fechaC = true;
+			}
+
+			if (getAño() > 0) {
+
+				fechaC = true;
+			}
+
+		}
+
+		
+		return fechaC;
+
+	}
+
+	public void diaSiguiente() {
+
+
+		if (getDia() > 31&&getMes()!=02) {
+
+			this.dia = 01;
+			this.mes++;
+			
+			if (getMes() > 12) {
+
+				this.mes = 01;
+				this.año++;
+			}
+
+		} else if ((esBisiesto()&&this.dia==29&&this.mes==02)) {
+			
+			this.dia=01;
+			this.mes= 03;
+			
+		} else if ((!esBisiesto()&&this.dia==29&&this.mes==02)) {
+			
+			this.dia=01;
+			this.mes= 03;
+			
+		}
 	
-	public Fecha (int dia, int mes, int año)          {
+	}
+
+	public String toString() {
+
+		String formato="0";
+		String dia =String.valueOf(getDia());
+		String mes= String.valueOf(getMes());
+		String año=String.valueOf(getAño());
+		String result;
 		
+		if (getDia()<10) {
+			
+			dia=formato+String.valueOf(getDia());
+			
+		}
 		
-		
+		if (getMes()<10) {
+			
+			mes=formato+String.valueOf(getMes());
+		}
+
+		result = dia+"/"+mes+"/"+"/"+año;
+
+		return result;
 	}
 
 }
--- Tema7_B2/src/ejercicio03/Main.java
+++ Tema7_B2/src/ejercicio03/Main.java
@@ -1,9 +1,62 @@
 package ejercicio03;
 
+import java.util.Scanner;
+
+import ejercicio01.Hora;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+
+		/*Creamos un escáner para poder leer los datos que introduzca el usuario por
+		la consola.*/
+										
+		Scanner sc= new Scanner (System.in); 
+		
+		//Creamos el objeto.
+		Fecha fecha= new Fecha();
+										
+		/*Le pediremos al usuario que introduzca los datos y los guardaremos en la 
+		* variable correspondiente.*/
+										
+		System.out.print("Introduzca el día y pulse Intro: ");
+		int dia= sc.nextInt();
+		System.out.print("Introduzca el mes y pulse Intro: ");
+		int mes= sc.nextInt();
+		System.out.print("Introduzca el año y pulse Intro: ");
+		int año= sc.nextInt();
+		
+		fecha.setDia(dia);
+		fecha.setMes(mes);
+		fecha.setAño(año);
+		
+	
+		
+		if (fecha.fechaCorrecta()) {
+			System.out.println(fecha);
+			
+			
+		} else {
+			
+			System.out.println("La fecha introducida no es válida.");
+		}
+		
+		if (!fecha.esBisiesto()) {
+			
+			System.out.println("No es bisiesto.");
+			
+		} else {
+			
+			System.out.println("Es bisiesto.");
+		}
+		
+		System.out.println("Si le añadimos un día resulta: ");
+		
+		fecha.diaSiguiente();
+		
+		System.out.println(fecha);
+		
 
 	}
 
